a693f067b41266c9a55e5dcf8723734b4dc25438
Saravana-05 <saravana.kumar@skylimitdigital.com>
2026-07-07 11:25:33 +0530

    route changed

--- src/routes/Router.jsx
+++ src/routes/Router.jsx
@@ -25,7 +25,7 @@ export const ROUTES = {
 
 export default function AppRouter({ user, onLogin, onLogout }) {
   return (
-    <BrowserRouter basename="/medics-v2/">
+    <BrowserRouter basename="/">
       <Routes>
         {/* Login Route - Home */}
         <Route 
